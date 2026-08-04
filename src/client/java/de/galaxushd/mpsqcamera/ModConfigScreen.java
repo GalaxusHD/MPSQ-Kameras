0493b86d688db6c1f76f9c44c06205da37d45f30
GalaxusHD <lanckau.jannis@icloud.com>
2026-08-04 16:23:43 +0200

    Update logo texture size to 864 pixels
    Increased the logo texture size from 860 to 864 pixels.

--- src/client/java/de/galaxushd/mpsqcamera/ModConfigScreen.java
+++ src/client/java/de/galaxushd/mpsqcamera/ModConfigScreen.java
@@ -11,7 +11,7 @@ import net.minecraft.util.Identifier;
 public class ModConfigScreen extends Screen {
 	private static final Identifier LOGO_TEXTURE =
 			Identifier.of(MpsqCameraClient.MOD_ID, "textures/gui/mpsqlogo.png");
-	private static final int LOGO_TEXTURE_SIZE = 860;
+	private static final int LOGO_TEXTURE_SIZE = 864;
 	private static final int LOGO_MAX_SIZE = 192;
 	private static final int LOGO_TOP_MARGIN = 8;
 	private static final int LOGO_BOTTOM_MARGIN = 12;
@@ -150,6 +150,8 @@ public class ModConfigScreen extends Screen {
 				0,
 				logoSize,
 				logoSize,
+				LOGO_TEXTURE_SIZE,
+				LOGO_TEXTURE_SIZE,
 				LOGO_TEXTURE_SIZE,
 				LOGO_TEXTURE_SIZE
 		);
