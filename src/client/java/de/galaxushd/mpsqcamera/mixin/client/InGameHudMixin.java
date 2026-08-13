e614b8410e88b82293ddfe07f48a73e2826eeba9
GalaxusHD <lanckau.jannis@icloud.com>
2026-08-13 04:48:37 +0200

    Add files via upload

---
+++ src/client/java/de/galaxushd/mpsqcamera/mixin/client/InGameHudMixin.java
@@ -0,0 +1,20 @@
+package de.galaxushd.mpsqcamera.mixin.client;
+
+import de.galaxushd.mpsqcamera.ScreenCreationManager;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderTickCounter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public final class InGameHudMixin {
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    private void mpsq$hideHudForCamera(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+        if (ScreenCreationManager.isCameraViewActive()) {
+            ci.cancel();
+        }
+    }
+}
